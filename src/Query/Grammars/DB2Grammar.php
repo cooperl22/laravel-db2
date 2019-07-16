f9222c47c3e740f5db7e122346fd2cd399e19e6c
Maxime Rault <maxime.rault@cooperl.com>
2019-07-16 09:42:20 +0200

    add dropColumn schema migration feature

--- src/Query/Grammars/DB2Grammar.php
+++ src/Query/Grammars/DB2Grammar.php
@@ -105,6 +105,13 @@ class DB2Grammar extends Grammar
 
         $components['columns'] = $this->compileOver($orderings, $columns);
 
+        // if there are bindings in the order, we need to move them to the select since we are moving the parameter
+        // markers there with the OVER statement
+        if(isset($query->getRawBindings()['order'])){
+            $query->addBinding($query->getRawBindings()['order'], 'select');
+            $query->setBindings([], 'order');
+        }
+
         unset($components['orders']);
 
         // Next we need to calculate the constraints that should be placed on the query
@@ -191,7 +198,7 @@ class DB2Grammar extends Grammar
 
         $existsQuery->columns = [];
 
-        return $this->compileSelect($existsQuery->selectRaw('1')->limit(1));
+        return $this->compileSelect($existsQuery->selectRaw('1 exists')->limit(1));
     }
 
     /**
